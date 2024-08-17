f8dbff091a2e73006559f93ab8ca7d13e1510ec3
Unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2024-08-17 11:43:52 +0000

    puppet fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,28 +1,5 @@
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-file {'/var/www/html':
-	ensure => directory,
-	owner => 'www-data',
-	group => 'www-data',
-}
-
-file {$file_to_edit:
-	ensure => file,
-	content => "<?php\n// wordPress settings\n\n// phpp typo causing Apache error\n\n?>",
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0644',
-}
-
-exec {'repace_typo_in_wp_settings':
-	command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+exec {'fix-apache-error':
+	command => '/usr/bin/chown www-data:www-data /var/www/html',
 	path => ['/bin', '/usr/bin'],
-	onlyif => "grep 'phpp' ${file_to_edit}",
-	require => File[$file_to_edit],
-}
-
-service {'apache2':
-	ensure => running,
-	enable => true,
-	require => Exec['replace_typo_in_wp_settings'],
+	unless => '/usr/bin/test -w /var/www/html',
 }
