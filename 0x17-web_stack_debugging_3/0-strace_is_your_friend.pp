03e31b454d71cf84cf9d8e29c67152e051f8cdf5
Unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2024-08-17 12:37:20 +0000

    puppet fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,5 +6,12 @@ $file_to_edit = '/var/www/html/wp-settings.php'
 
 exec {'fix-wordpress':
 	command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-	path => ['/bin', '/usr/bin']
+	path => ['/bin', '/usr/bin'],
+	onlyif => "grep -q 'phpp' ${file_to_edit}",
+}
+
+service {'apache2':
+	ensure => 'running',
+	subscribe => Exec['fix-wordpress'],
+	require => Exec['fix-wordpress'],
 }
